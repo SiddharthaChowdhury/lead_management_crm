0de10eca1501828b9f05232073cd3e7a578a36b7
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-03 14:33:59 +0530

    Message changed

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -2,7 +2,7 @@
 module.exports = {
 
 	dash_view: function(req, res){
-		return res.view('superadmin/dashboard', {message:'Success! Institute created '+req.session.user.admin.name+' is admin.' });
+		return res.view('superadmin/dashboard', {message:'Success! Institute created, '+req.session.user.admin.name+' is admin.' });
 	},
 
 };
