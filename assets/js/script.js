ab673954c79a2f12d4c4f2e3a86088b967dff7e2
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-08 15:09:30 +0530

    To some creation of institute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-$(function(){
-	
-})
+//	~~~ LIBRARY ~~~~~
+	function _(x){return document.getElementById(x);}
+	var count = 0;
+//	~~~~~~~~~~~~~~~~~~~
+
+
+// DASHBOARD 
+										//(create new branch)
+$('.jq_new_branch').click(function(e){
+	e.preventDefault();
+	var container = $('#si_new_branches');
+	var clone = $('.si_new_block').clone().first()
+	clone.find('.form-control').empty();
+	count++;
+	if( clone.hasClass('hide-this') ) clone.removeClass('hide-this')
+	clone.find('span').text(count)
+	container.append(clone);
+});
+
+
